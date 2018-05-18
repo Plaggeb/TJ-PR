4a04db77c035489e7a2aed666868e06447d1de62
Brandon.Plagge <Brandon.Plagge@SAT-LT-BRANDON>
2018-05-18 13:41:44 -0500

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -11,17 +13,31 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { PRGatewayService } from '../services/prGateway.service';
+import { RequisitionService } from '../services/requisition.service';
+import { CriteriaPage } from '../pages/criteria/criteria';
+import { DetailPage } from '../pages/detail/detail';
+import { AuthPage } from '../pages/auth/auth';
+
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
+    CriteriaPage,
+    DetailPage,
+    AuthPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    JsonpModule,
+    HttpModule,
+    HttpClientModule
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +45,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    CriteriaPage,
+    DetailPage,
+    AuthPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PRGatewayService,
+    RequisitionService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,183 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController, AlertController, LoadingController } from 'ionic-angular';
+
+import { PRGatewayService } from '../../services/prGateway.service';
+import { RequisitionService } from '../../services/requisition.service';
+import { CriteriaPage } from '../criteria/criteria';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { IRequisition } from '../../interfaces/requisition.interface';
+import { DetailPage } from '../detail/detail';
+import { HttpErrorResponse } from '@angular/common/http';
+import { IReturnMessage } from '../../interfaces/returnMessage.interface';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  selectAll: boolean = false;
+  relCode: string = "";
+  relGroup: string = "";
+  approvalForm: FormGroup;
+  PRs: IRequisition[] = [];
 
+  constructor(private navCtrl: NavController, 
+    private prService: PRGatewayService, 
+    private reqService: RequisitionService,
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) {
   }
 
+  ngOnInit() {
+    if (this.relCode == "" || this.relGroup == "") {
+      this.onSetCodeAndGroup().then(() => {
+        this.initializeForm();
+      });
+    } 
+  }
+
+  // Changes the value of selectAll to the opposite of what it currently is.
+  // This change updates the bound checkboxes checked value.
+  onSelectAll() {
+    this.selectAll = !this.selectAll;
+  }
+
+  // Scroll to top to refresh page
+  onRefresher(refresher) {
+    this.refreshPRs().then(() => {
+      refresher.complete();
+    });
+  }
+
+  //  Set the release code and release group, need to test if * can be sent for release group
+  onSetCodeAndGroup(): Promise<any> {
+    const promise = new Promise((resolve, reject) => {
+      const modal = this.modalCtrl.create(CriteriaPage, {'relCode': this.relCode, 'relGroup': this.relGroup} );
+      modal.present();    
+      modal.onDidDismiss(data => {
+        this.relCode = (<string>data.relCode).toUpperCase();
+        this.relGroup = (<string>data.relGroup).toUpperCase();
+        this.refreshPRs().then(() => {
+          resolve({"updated": true});
+        });
+      });
+    });
+    return promise;
+  }
+
+  private initializeForm() {
+    let reqs: any[] = [];
+
+    if (this.PRs.length > 0) {
+      for (let req of this.PRs) {
+        reqs.push(new FormControl(false));
+      }
+    }
+
+    this.approvalForm = new FormGroup({
+      "requisitions": new FormArray(reqs, Validators.required)
+    });
+  }
+
+  // Refreshes the PR array
+  refreshPRs(): Promise<any> {
+    const loading = this.loadingCtrl.create({content: 'Fetching PRs'});
+    try {
+      loading.present();
+      const promise = new Promise((resolve, reject) =>{
+        this.prService.getReleaseItems(this.relGroup, this.relCode).subscribe(
+          (data: any) => {
+            console.log(data);
+            this.reqService.clear();
+            if ((<IRequisition[]>data['REQUISITION_ITEMS']).length > 0) {
+              this.reqService.addItems((<IRequisition[]>data['REQUISITION_ITEMS']));
+            } else {
+              let  temp: IReturnMessage = (<IReturnMessage>data['RETURN'][0]);
+              const alert = this.alertCtrl.create({
+                message: "<B>Return Code: </B>" + temp.CODE + "<BR><B>Message: </B>" + temp.MESSAGE,
+                buttons: ['OK']
+              });
+              alert.present();
+            }
+            this.PRs = this.reqService.getItems();
+            this.initializeForm();
+            resolve({"updated": true});
+            loading.dismiss();
+          },
+          (err: any) => {
+            loading.dismiss();
+            
+            const alert = this.alertCtrl.create({
+                    message: "Observable Error: " + err,
+                    buttons: ['OK']
+                });
+            alert.present();
+            resolve({"updated": false});
+          }
+        );
+      });
+      return promise;
+    } 
+    catch (error) 
+    {
+      //loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title: 'Exception in RefreshPRs',
+        message: error.message,
+        buttons: ['Okay']
+      });
+      alert.present();
+    }
+  }
+
+  onSubmit() {
+    let x = this.approvalForm.value["requisitions"].length - 1;
+    for (x; x >= 0; x--) {
+      if (this.approvalForm.value["requisitions"][x]) {
+        this.prService.releaseItem(this.PRs[x].PREQ_NO, this.PRs[x].PREQ_ITEM, "TC").subscribe((response: any) => {
+          console.log(response);
+          this.reqService.removeItem(x);
+        });
+      }
+    }
+    this.PRs = this.reqService.getItems();
+    this.selectAll = false;
+    this.initializeForm();
+  }
+
+  onDetails(index: number) {
+    this.navCtrl.push(DetailPage, {"pr": this.PRs[index]});
+  }
+
+  onApproval(index: number) {
+    let pr: IRequisition = this.PRs[index];
+    const alert = this.alertCtrl.create({
+      title: 'Approve PR',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to approve PR: ' + pr.PREQ_NO + ' Line: ' + pr.PREQ_ITEM + '?',
+      buttons: [{
+        text: 'Yes, go ahead',
+        handler: () => {
+          // Approve PR wrap below in the observable
+          this.prService.releaseItem(pr.PREQ_NO, pr.PREQ_ITEM, "TC").subscribe((response: any) => {
+            console.log(response);
+            // Remove from service array if successfully approved
+            this.reqService.removeItem(index);
+            // Refresh local array
+            this.PRs = this.reqService.getItems();
+            // Refresh form
+            this.initializeForm();
+          });
+        }
+      }, {
+        text: 'No, I changed my mind!',
+        role: 'Cancel',
+        handler: () => { console.log('Nope'); }
+      }]
+    });
+    alert.present();
+  }
 }
